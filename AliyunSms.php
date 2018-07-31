230d0ddb88c3c7f40153b997d68085ca2426b58f
listen <taghui@37du.xin>
2018-07-31 14:11:48 +0800

    第一次正式提交

--- AliyunSms.php
+++ AliyunSms.php
@@ -3,32 +3,162 @@
  * User: Listen
  * Date: 2018/7/30
  * Time: 下午6:21
- * Description:
+ * Description: 阿里云短信发送
  * version: 0.0.1
  */
 
-namespace Aliyun\AliyunSms;
+namespace Aliyun;
 
 use Aliyun\Core\Config;
 use Aliyun\Core\Profile\DefaultProfile;
 use Aliyun\Core\DefaultAcsClient;
 use Aliyun\Api\Sms\Request\V20170525\SendSmsRequest;
-use Aliyun\Api\Sms\Request\V20170525\SendBatchSmsRequest;
-use Aliyun\Api\Sms\Request\V20170525\QuerySendDetailsRequest;
 
 // 加载区域结点配置
 Config::load();
 
 class AliyunSms
 {
-    static $acsClient = null;
+    static  $acsClient = null;
+    private $accessKeyid;
+    private $accessKeySecre;
+    private $SignName;
+    private $TemplateCode;
+    private $TemplateParam;
+    private $OutId;
+    private $SmsUpExtendCode;
+
+
+    /**
+     * @return mixed
+     */
+    public function getAccessKeyid()
+    {
+        return $this->accessKeyid;
+    }
+
+    /**
+     * @param mixed $accessKeyid
+     */
+    public function setAccessKeyid($accessKeyid)
+    {
+        $this->accessKeyid = $accessKeyid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccessKeySecre()
+    {
+        return $this->accessKeySecre;
+    }
+
+    /**
+     * @param mixed $accessKeySecre
+     */
+    public function setAccessKeySecre($accessKeySecre)
+    {
+        $this->accessKeySecre = $accessKeySecre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSignName()
+    {
+        return $this->SignName;
+    }
+
+    /**
+     * @param mixed $SignName
+     */
+    public function setSignName($SignName)
+    {
+        $this->SignName = $SignName;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTemplateParam()
+    {
+        return $this->TemplateParam;
+    }
+
+    /**
+     * @param array $TemplateParam
+     */
+    public function setTemplateParam($TemplateParam)
+    {
+        $this->TemplateParam = $TemplateParam;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTemplateCode()
+    {
+        return $this->TemplateCode;
+    }
+
+    /**
+     * @param mixed $TemplateCode
+     */
+    public function setTemplateCode($TemplateCode)
+    {
+        $this->TemplateCode = $TemplateCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOutId()
+    {
+        return $this->OutId;
+    }
+
+    /**
+     * @param string $OutId
+     */
+    public function setOutId($OutId)
+    {
+        $this->OutId = $OutId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSmsUpExtendCode()
+    {
+        return $this->SmsUpExtendCode;
+    }
+
+    /**
+     * @param mixed $SmsUpExtendCode
+     */
+    public function setSmsUpExtendCode($SmsUpExtendCode)
+    {
+        $this->SmsUpExtendCode = $SmsUpExtendCode;
+    }
+
+
+    public function __construct($accessKeyid,$accessKeySecre,$SignName,$TemplateCode,$OutId='',$SmsUpExtendCode)
+    {
+        $this->accessKeyid = $accessKeyid;
+        $this->accessKeySecre = $accessKeySecre;
+        $this->SignName = $SignName;
+        $this->TemplateCode = $TemplateCode;
+        $this->OutId = $OutId;
+        $this->SmsUpExtendCode = $SmsUpExtendCode;
+    }
+
 
     /**
      * 取得AcsClient
      *
      * @return DefaultAcsClient
      */
-    public static function getAcsClient() {
+    public  function getAcsClient() {
         //产品名称:云通信流量服务API产品,开发者无需替换
         $product = "Dysmsapi";
 
@@ -36,9 +166,9 @@ class AliyunSms
         $domain = "dysmsapi.aliyuncs.com";
 
         // TODO 此处需要替换成开发者自己的AK (https://ak-console.aliyun.com/)
-        $accessKeyId = "yourAccessKeyId"; // AccessKeyId
+        $accessKeyId = $this->accessKeyid; // AccessKeyId
 
-        $accessKeySecret = "yourAccessKeySecret"; // AccessKeySecret
+        $accessKeySecret = $this->accessKeySecre; // AccessKeySecret
 
         // 暂时不支持多Region
         $region = "cn-hangzhou";
@@ -65,7 +195,7 @@ class AliyunSms
      * 发送短信
      * @return stdClass
      */
-    public static function sendSms() {
+    public function sendSms($tel,$TemplateParam) {
 
         // 初始化SendSmsRequest实例用于设置发送短信的参数
         $request = new SendSmsRequest();
@@ -74,119 +204,31 @@ class AliyunSms
         //$request->setProtocol("https");
 
         // 必填，设置短信接收号码
-        $request->setPhoneNumbers("12345678901");
+        $request->setPhoneNumbers($tel);
 
         // 必填，设置签名名称，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
-        $request->setSignName("短信签名");
+        $request->setSignName($this->SignName);
 
         // 必填，设置模板CODE，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
-        $request->setTemplateCode("SMS_0000001");
+        $request->setTemplateCode($this->TemplateCode);
 
         // 可选，设置模板参数, 假如模板中存在变量需要替换则为必填项
-        $request->setTemplateParam(json_encode(array(  // 短信模板中字段的值
-            "code"=>"12345",
-            "product"=>"dsd"
-        ), JSON_UNESCAPED_UNICODE));
+        //判断是否为有效值 数组格式为 "array("key"=>"value")"
+        if(is_array($TemplateParam) && isset($TemplateParam))
+        {
+            $request->setTemplateParam(json_encode($TemplateParam, JSON_UNESCAPED_UNICODE));
+        }
 
         // 可选，设置流水号
-        $request->setOutId("yourOutId");
+        $request->setOutId($this->OutId);
 
         // 选填，上行短信扩展码（扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段）
-        $request->setSmsUpExtendCode("1234567");
+        $request->setSmsUpExtendCode($this->SmsUpExtendCode);
 
         // 发起访问请求
-        $acsResponse = static::getAcsClient()->getAcsResponse($request);
+        $acsResponse = $this->getAcsClient()->getAcsResponse($request);
 
         return $acsResponse;
-    }
-
-    /**
-     * 批量发送短信
-     * @return stdClass
-     */
-    public static function sendBatchSms() {
-
-        // 初始化SendSmsRequest实例用于设置发送短信的参数
-        $request = new SendBatchSmsRequest();
-
-        //可选-启用https协议
-        //$request->setProtocol("https");
-
-        // 必填:待发送手机号。支持JSON格式的批量调用，批量上限为100个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
-        $request->setPhoneNumberJson(json_encode(array(
-            "1500000000",
-            "1500000001",
-        ), JSON_UNESCAPED_UNICODE));
-
-        // 必填:短信签名-支持不同的号码发送不同的短信签名
-        $request->setSignNameJson(json_encode(array(
-            "云通信",
-            "云通信"
-        ), JSON_UNESCAPED_UNICODE));
-
-        // 必填:短信模板-可在短信控制台中找到
-        $request->setTemplateCode("SMS_1000000");
-
-        // 必填:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-        // 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-        $request->setTemplateParamJson(json_encode(array(
-            array(
-                "name" => "Tom",
-                "code" => "123",
-            ),
-            array(
-                "name" => "Jack",
-                "code" => "456",
-            ),
-        ), JSON_UNESCAPED_UNICODE));
-
-        // 可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
-        // $request->setSmsUpExtendCodeJson("[\"90997\",\"90998\"]");
-
-        // 发起访问请求
-        $acsResponse = static::getAcsClient()->getAcsResponse($request);
-
-        return $acsResponse;
-    }
-
-    /**
-     * 短信发送记录查询
-     * @return stdClass
-     */
-    public static function querySendDetails() {
-
-        // 初始化QuerySendDetailsRequest实例用于设置短信查询的参数
-        $request = new QuerySendDetailsRequest();
-
-        //可选-启用https协议
-        //$request->setProtocol("https");
-
-        // 必填，短信接收号码
-        $request->setPhoneNumber("12345678901");
-
-        // 必填，短信发送日期，格式Ymd，支持近30天记录查询
-        $request->setSendDate("20170718");
-
-        // 必填，分页大小
-        $request->setPageSize(10);
-
-        // 必填，当前页码
-        $request->setCurrentPage(1);
-
-        // 选填，短信发送流水号
-        $request->setBizId("yourBizId");
-
-        // 发起访问请求
-        $acsResponse = static::getAcsClient()->getAcsResponse($request);
-
-        return $acsResponse;
-    }
-
-
-    public static function Test()
-    {
-        echo 'Test';
-        exit;
     }
 
 }
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Aliyun SMS Sending
 
 阿里云短信发送 API 整合进Yii2
+
+
+###License
+除 “版权所有（C）阿里云计算有限公司” 的代码文件外，遵循 [MIT license](http://opensource.org/licenses/MIT) 开源。
